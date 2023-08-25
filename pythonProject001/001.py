224c5f3000779c3d36a2752bbf69f7c6cedb4f71
zhiyongzhou <Zhiyong.Zhou@metropolia.fi>
2023-08-25 15:59:06 +0800

    20230925 9-11.20am KMC557

---
+++ pythonProject001/001.py
@@ -0,0 +1,51 @@
+# # intVar = 0;
+# # print(intVar);
+# #
+# # strVar = "zzzzzzzzzzzzzzz01";
+# # print(strVar);
+# #
+# # floatVar = 1.0;
+# # print(floatVar);
+# # print(type(floatVar));
+# # print()
+# # print()
+# # print()
+# # print()
+# #
+# # # balabalabalabalabala   jijirulVling
+# # result = int(intVar + floatVar)
+# # print(result)
+# # print(type(result))
+# #
+# # print()
+# # print()
+# # print("------------------------------------------")
+# #
+# # shareOfLoan = 800.50 / 3
+# # print(shareOfLoan)
+# # print(int(shareOfLoan))
+# #
+# # print("111\n2222")
+# #
+# # num1 = input("enter some Value: ")
+# # num2 = input("Enter the second Value: ")
+# # result2 = int(num1) + int(num2);
+# # print("result is:\t", result2)
+# #
+# name = input("what's ur name: ");
+# age = input("ur age please: ");
+# print("name: ", name, " age: ", age);
+# outPut = f"name: {name} Age: {age}";
+# print(outPut);
+
+
+import math
+
+pi = 3.14159265;
+formatPi = "value of PI: {:.5f}".format(math.pi)
+print(formatPi)
+
+# radius = float(input("input somethings:"))
+# areaCircle = pi *radius * radius
+# print("the areaCircle result is:{:.2f}".format(areaCircle))
+# print(f"the areaCircle result is:{areaCircle:.4f}")
