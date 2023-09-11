0b91e4eaec3b2aff3460f28e33f9ecbf448a6c7a
zhiyongzhou <Zhiyong.Zhou@metropolia.fi>
2023-09-11 15:31:16 +0800

    Assignment: Python - Module 3

--- pythonProject001/assignment/Python - Module 3_zhiyongZhou.py
+++ pythonProject001/assignment/Python - Module 3_zhiyongZhou.py
@@ -52,7 +52,7 @@ elif (gender == "males"):
 # A year is a leap year if it is divisible by four.
 # However, years divisible by 100 are leap years only if they are also divisible by 400.
 year = int(input("enter a year: "));
-if((year % 4) == 0 and (year % 400) == 0):
+if((year % 4) == 0 and (year % 100) != 0) or (year % 400) == 0:
     print("the year is a leap year ");
 else:
     print("the year is not a leap year ");
