5291b0124b29a6653a17d69d47d55ec92fa24a11
zhiyongzhou <Zhiyong.Zhou@metropolia.fi>
2023-09-27 15:41:48 +0800

    mudule 8 assignment

--- pythonProject001/assignment/Python - Module 8_zhiyongZhou.py
+++ pythonProject001/assignment/Python - Module 8_zhiyongZhou.py
@@ -18,19 +18,21 @@ def getSqlResult(sql):
     return cursor.fetchall();
 
 
-# Kirjoita ohjelma, joka kysyy käyttäjältä lentoaseman ICAO-koodin.
-# Ohjelma hakee ja tulostaa koodia vastaavan lentokentän nimen ja sen sijaintikunnan kurssilla käytettävästä lentokenttätietokannasta.
-# ICAO-koodi on tallennettuna airport-taulun ident-sarakkeeseen.
+# Write a program that asks the user to enter the ICAO code of an airport.
+# The program fetches and prints out the corresponding airport name
+# and location (town) from the airport database used on this course.
+# The ICAO codes are stored in the ident column of the airport table.
 ICAO_code = input("input ICAO code: ");
-sql = "select `name`, latitude_deg, longitude_deg from airport where ident = '" + ICAO_code + "'";
+sql = "select `name`, municipality from airport where ident = '" + ICAO_code + "'";
 result = getSqlResult(sql);
 if cursor.rowcount > 0:
     for row in result:
-        print(f"airport name is {row[0]}, it location is {row[1]}, {row[2]}");
+        print(f"airport name is {row[0]}, it location is {row[1]}");
 
-# Write a program that asks the user for the country code (for example FI) and prints the number of airports in that country by type.
-# For Finland, for example, the result must be information about
-# that there are 65 small airports, 15 heliports, etc.
+
+# Write a program that asks the user to enter the area code (for example FI)
+# and prints out the airports located in that country ordered by airport type.
+# For example, Finland has 65 small airports, 15 helicopter airports and so on.
 countryCode = input("put country code: ");
 sql = "select count(0), a.type from country c left join airport a on a.iso_country = c.iso_country where c.iso_country = '" + countryCode + "' GROUP BY a.type"
 cursor.execute(sql);
@@ -39,14 +41,17 @@ str = "there are ";
 if (cursor.rowcount > 0):
     for row in result:
         str = str + f"{row[0]} {row[1]}, ";
-str = str + 'etc.';
+str = str + 'and so on';
 print(str);
 
-# Write a program that prompts the user for the ICAO codes of two airports.
-# The program indicates the distance between the airports in kilometers.
-# The calculation is based on the coordinates retrieved from the database.
+
+
+# Write a program that asks the user to enter the ICAO codes of two airports.
+# The program prints out the distance between the two airports in kilometers.
+# The calculation is based on the airport coordinates fetched from the database.
 # Calculate the distance using the geopy library: https://geopy.readthedocs.io/en/stable/.
-# Install the library by choosing View / Tool Windows / Python Packages. Enter geopy in the search field and complete the installation.
+# Install the library by selecting View / Tool Windows / Python Packages in your PyCharm IDE,
+# write geopy into the search field and finish the installation.
 ICAO_code_first = input("input ICAO code firstly: ") + "'";
 ICAO_code_second = input("input ICAO code secondly: ") + "'";
 sql = "select latitude_deg, longitude_deg, `name` from airport where ident = '";
